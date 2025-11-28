80fb0a6e93b3432dd0d22bc9faf389fb8f9c2271
Chris <forklift2flutter@gmail.com>
2025-11-27 22:05:54 -0500

    update 01.1_Concatination.dart

--- 01.1_Concatination.dart
+++ 01.1_Concatination.dart
@@ -2,10 +2,9 @@ void main() {
   print("Concatenation in Dart:");
   print("Let's talk about that gatekeper of a word.");
   print("I've been an English speaker all my life (well, I skipped a few of the early years), and I've never heard of this word.");
-  print("That's why I'm calling it a gatekeper. It discourages outsiders, makes peole think that coding is magic and beyond them.");
-
-
-
+  print("I have smart friends, friends with PhDs, friends who are writers, friends who are teachers, and they haven't ever used this word.");
+  print("That's why I'm calling it a gatekeper. It discourages outsiders, makes peole think that coding is magic, that it's beyond them.");
+  print("I looked up the word on the Oxford English Dictionary website and they show the use of the word begins in the 1600s, then drops, only to revive in the 1950s as computer science began to grow.");
 
 
 
